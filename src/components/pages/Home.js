06850c4a8beafaf0a42d0c630c69503040413fe9
Rodrigo-EF-Neves <rodrigo.emilio2004@gmail.com>
2023-05-15 14:47:54 -0300

    Desenvolvendo a Home #2

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,32 @@
 import styles from './Home.module.css'
 import FestivalDeDanca from '../../imgs/festival_de_dança_news.jpg'
+import Libertadores from '../../imgs/libertadores_news.jpg'
 
 function Home(){
     
     return (
-        <section className={styles.home_container}>
-            <div className={styles.main_news}>
-                <img src={FestivalDeDanca} alt="Festival de Dança de Joinville"/>
-                <div className={styles.main_news_title}>
-                    40° Festival de Dança de Joinville
+        <div>
+            <section className={styles.home_container}>
+                <div className={styles.main_news}>
+                    <img src={FestivalDeDanca} alt="Festival de Dança de Joinville"/>
+                    <div className={styles.main_news_title}>
+                        40° Festival de Dança de Joinville
+                    </div>
+                    <div className={styles.main_news_description}>
+                        Atrações, apresentações e datas
+                    </div>
                 </div>
-                <div className={styles.main_news_description}>
-                    Atrações, apresentações e datas
+            </section>
+            <hr className={styles.horizontal_line}/>
+            <section className={styles.home_news_container}>
+                <div className={styles.home_news_img}>
+                   <img src={Libertadores} alt="Libertadores da América"/>
                 </div>
-            </div>
-        </section>
+                <div className={styles.home_news_text}>
+                    <h3>Confira os jogos da Libertadores da América</h3>
+                </div>
+            </section>
+        </div>
     )
 }
 
