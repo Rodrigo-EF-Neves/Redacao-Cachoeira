13c5e5e3a8a175995fb7ed8920a1b8005f326405
Rodrigo-EF-Neves <rodrigo.emilio2004@gmail.com>
2023-05-18 14:34:39 -0300

    Iniciando o desenvolvimento da página de notícias

--- src/components/pages/News.js
+++ src/components/pages/News.js
@@ -1,5 +1,18 @@
 function News() {
-
+    return (
+        <main>
+            <section>
+                <div>
+                    <div>
+                        Imagem da notícia
+                    </div>
+                    <div>
+                        Informações da notícia
+                    </div>
+                </div>
+            </section>
+        </main>
+    )
 }
 
 export default News;
